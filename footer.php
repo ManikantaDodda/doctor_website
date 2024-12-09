51d47d41b2510cd86b63fa675e7462e64eae91a6
ManikantaDodda <nagasaidodda@gmail.com>
2024-12-09 15:11:21 +0530

    assets

---
+++ footer.php
@@ -0,0 +1,79 @@
+<footer class="footer">
+    <div class="footer__group">
+        <div class="footer__group-top">
+            <div class="footer__group-images">
+                <img src="public/images/img_frame.png" alt="Frame Image" class="footer__image-frame" />
+                <img src="public/images/img_logo_1.png" alt="Logo Image" class="footer__image-logo" />
+            </div>
+            <div class="footer__group-info">
+                <p class="footer__text-doctor ui text size-textlg">
+                    nee<br />replacement surgery. leading knee replacement surgeons in<br />Hyderabad with over 23+
+                    years of expertise in orthopedics &
+                </p>
+                <div class="footer__group-address">
+                    <div class="footer__group-address-label">
+                        <p class="footer__text-address-label ui text size-textlg">Address</p>
+                    </div>
+                    <p class="footer__text-address ui text size-textlg">
+                        5th Floor, Above Sachdev Sports, Sri Tirumala’s Subash Arcade, H.1-62/1 Kondapur, X Roads,
+                        Kothaguda,
+                    </p>
+                </div>
+                <div class="footer__group-social">
+                    <div class="footer__group-social-links">
+                        <img src="public/images/img_linkedin.svg" alt="Linkedin Icon" class="footer__image-call" />
+                        <p class="footer__text-phone ui text size-textlg">Address</p>
+                    </div>
+                    <div class="footer__line">
+                        <p class="footer__text-address ui text size-textlg">
+                            Plot 101/D, RISE Commercial Complex, 3rd Floor, Over Van Lavino Cafe, opp. to APARNA SAROVAR
+                            GRANDE,
+                        </p>
+                    </div>
+                </div>
+                <div class="footer__group-contact">
+                    <img src="public/images/img_call.svg" alt="Phone Icon" class="footer__image-call" />
+                    <p class="footer__text-phone ui text size-textmd">+91 - 9137464646</p>
+                </div>
+                <div class="footer__group-email">
+                    <img src="public/images/img_lock.svg" alt="Email Icon" class="footer__image-call" />
+                    <p class="ui text size-textlg">abc@gmail.com</p>
+                </div>
+
+            </div>
+            div class="footer__group-services"> <img src="public/images/img_frame_red_a700.png" alt="Red Frame Image"
+                class="footer__image-frame-red" />
+            <div class="footer__group-services-list">
+                <h5 class="ui heading size-headinglg">Our Services</h5>
+                <ul class="footer__list">
+                    <li> <a href="#">
+                            <p class="ui text size-textlg">TKR Surgery</p>
+                        </a> </li>
+                    <li> <a href="#" class="thr-surgery-link-link">
+                            <p class="ui text size-textlg">THR Surgery</p>
+                        </a> </li>
+                    <li> <a href="Arthroscopy" target="_blank" rel="noreferrer" class="arthroscopy-link-link">
+                            <p class="ui text size-textlg">Arthroscopy</p>
+                        </a> </li>
+                    <li> <a href="#" class="arthroscopy-link-link">
+                            <p class="ui text size-textlg">Sports Medicine</p>
+                        </a> </li>
+                    <li> <a href="#" class="general-ortho-link-link">
+                            <p class="ui text size-textlg">General Ortho</p>
+                        </a> </li>
+                    <li> <a href="#" class="thr-surgery-link-link">
+                            <p class="ui text size-textlg">Shoulder Arthroscopy</p>
+                        </a>
+                    </li>
+                    <li> <a href="#" class="arthroscopy-link-link">
+                            <p class="ui text size-textlg">Robotic Knee Replacement</p>
+                        </a> </li>
+                    <li> <a href="Trauma" target="_blank" rel="noreferrer" class="arthroscopy-link-link">
+                            <p class="ui text size-textlg">Trauma</p>
+                        </a> </li>
+                </ul>
+            </div>
+        </div>
+    </div> <img src="public/images/img_isolation_mode.png" alt="Isolation Mode Image"
+        class="footer__image-isolation-mode" /> </div>
+</footer>
